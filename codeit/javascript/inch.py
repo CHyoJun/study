849bd05d2c91ef5f3b36da8dd284c49e246cd555
CHyoJun <knock19950108@gmail.com>
2020-06-12 22:18:22 +0900

    new

--- codeit/javascript/inch.py
+++ codeit/javascript/inch.py
@@ -1,6 +1,6 @@
 function inchToCentimeter(inch) {
 	var centimeter = inch * 2.54; // 1 inch = 2.54cm
-	return centimeyer;			 // cm로 계산한 결괏값 돌려주기
+	return centimeter;			 // cm로 계산한 결괏값 돌려주기
 }
 
 var result1 = inchToCentimeter(2);  // 2 inch를 cm로 바꾼 값
